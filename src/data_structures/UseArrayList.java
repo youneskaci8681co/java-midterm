4b458eebefd967addc3fd414bb74e06acd03c0a1
youneskaci8681co <youneskaci388@yahoo.com>
2023-06-01 19:54:47 -0700

    working with array list

--- src/data_structures/UseArrayList.java
+++ src/data_structures/UseArrayList.java
@@ -1,17 +1,37 @@
-package data_structures;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class UseArrayList {
 
-    /** INSTRUCTIONS
-     *
-     * Demonstrate how to use an ArrayList that includes using the add, peek, remove & retrieve methods.
-     * Use For-Each loop and While-loop with Iterator to retrieve the data.
-     *
-     * Store and retrieve the data from/to a database table.
-     */
-
     public static void main(String[] args) {
+        List<String> data = new ArrayList<>();
 
+        // Adding elements to the ArrayList
+        data.add("Apple");
+        data.add("Banana");
+        data.add("Orange");
+
+        // Peeking an element (accessing without removing)
+        String firstElement = data.get(0);
+        System.out.println("First element: " + firstElement);
+
+        // Removing an element
+        boolean removed = data.remove("Banana");
+        System.out.println("Element removed: " + removed);
+
+        // Retrieving data using a for-each loop
+        System.out.println("Data retrieved using for-each loop:");
+        for (String element : data) {
+            System.out.println(element);
+        }
+
+
+        System.out.println("Data retrieved using while loop and Iterator:");
+        Iterator<String> iterator = data.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
     }
-
 }
