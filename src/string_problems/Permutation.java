9367312928c31a8f421c08b7b87ca18b07d849f0
youneskaci8681co <youneskaci388@yahoo.com>
2023-06-03 14:31:53 -0700

    working with permulation

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -1,15 +1,41 @@
 package string_problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutation {
 
-    /** INSTRUCTIONS
-     * Create a Java program to compute all permutations of any given string
-     *
-     * e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
-     */
-
     public static void main(String[] args) {
-
+        String str = "ABC";
+        List<String> permutations = computePermutations(str);
+        System.out.println(permutations);
     }
-    // Implement Here
+
+    public static List<String> computePermutations(String str) {
+        List<String> result = new ArrayList<>();
+        permute(str, 0, str.length() - 1, result);
+        return result;
+    }
+
+    private static void permute(String str, int left, int right, List<String> result) {
+        if (left == right) {
+            result.add(str);
+            return;
+        }
+
+        for (int i = left; i <= right; i++) {
+            str = swap(str, left, i);
+            permute(str, left + 1, right, result);
+            str = swap(str, left, i); // backtrack
+        }
+    }
+
+    private static String swap(String str, int i, int j) {
+        char[] charArray = str.toCharArray();
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
 }
+
