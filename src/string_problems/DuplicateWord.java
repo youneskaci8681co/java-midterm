22408a35a63191a170cd859fa8c5fa62c7b06b05
youneskaci8681co <youneskaci388@yahoo.com>
2023-06-03 14:29:40 -0700

    palindrom

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,17 +1,63 @@
 package string_problems;
 
-public class DuplicateWord {
+import java.util.HashMap;
+import java.util.Map;
 
-    /** INSTRUCTIONS
-     * Write a java program to find duplicate words and the number of occurrences of those words in the given string.
-     * Also, find the average length of all the words
-     */
+public class DuplicateWord {
 
     public static void main(String[] args) {
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
-        // Implement here
+        Map<String, Integer> wordOccurrences = findDuplicateWords(st);
+        double averageLength = calculateAverageLength(st);
 
+        // Print the duplicate words and their occurrences
+        System.out.println("Duplicate Words and Occurrences:");
+        for (Map.Entry<String, Integer> entry : wordOccurrences.entrySet()) {
+            String word = entry.getKey();
+            int occurrences = entry.getValue();
+            System.out.println(word + ": " + occurrences);
+        }
+
+        // Print the average length of all the words
+        System.out.println("Average Length of Words: " + averageLength);
     }
 
+    public static Map<String, Integer> findDuplicateWords(String st) {
+        Map<String, Integer> wordOccurrences = new HashMap<>();
+        String[] words = st.split("\\s+");
+
+        for (String word : words) {
+            // Remove punctuation marks and convert to lowercase
+            word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+            if (wordOccurrences.containsKey(word)) {
+                // If the word is already present, increment the count
+                int count = wordOccurrences.get(word);
+                wordOccurrences.put(word, count + 1);
+            } else {
+                // If the word is encountered for the first time, add it to the map
+                wordOccurrences.put(word, 1);
+            }
+        }
+
+        return wordOccurrences;
+    }
+
+    public static double calculateAverageLength(String st) {
+        String[] words = st.split("\\s+");
+        int totalLength = 0;
+
+        for (String word : words) {
+            // Remove punctuation marks
+            word = word.replaceAll("[^a-zA-Z0-9]", "");
+            totalLength += word.length();
+        }
+
+        if (words.length > 0) {
+            return (double) totalLength / words.length;
+        } else {
+            return 0;
+        }
+    }
 }
--- src/string_problems/Palindrome.java
+++ src/string_problems/Palindrome.java
@@ -2,18 +2,36 @@ package string_problems;
 
 public class Palindrome {
 
-    /** INSTRUCTIONS
-     * A palindrome is a word that can be reversed, and still remain the same.
-     *
-     * Example: MOM, DAD, MADAM, RACECAR
-     *
-     * Create a method to check if any given String is a palindrome or not.
-     */
-
     public static void main(String[] args) {
+        String word1 = "MOM";
+        System.out.println(isPalindrome(word1));  // true
 
+        String word2 = "DAD";
+        System.out.println(isPalindrome(word2));  // true
+
+        String word3 = "MADAM";
+        System.out.println(isPalindrome(word3));  // true
+
+        String word4 = "RACECAR";
+        System.out.println(isPalindrome(word4));  // true
+
+        String word5 = "HELLO";
+        System.out.println(isPalindrome(word5));  // false
     }
 
-    // Implement here
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
 
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
+
